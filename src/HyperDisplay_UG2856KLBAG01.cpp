907428e26b1e19ccc5b8d980baa1dc176dbb2764
Owen L - SFE <oclyke@gmail.com>
2019-02-25 13:52:57 -0700

    Initial commit

---
+++ src/HyperDisplay_UG2856KLBAG01.cpp
@@ -0,0 +1,301 @@
+#include "HyperDisplay_UG2856KLBAG01.h"
+
+////////////////////////////////////////////////////////////
+//				UG6464TDDBG01 Implementation			  //
+////////////////////////////////////////////////////////////
+char_info_t UG6464TDDBG01_Default_CharInfo;
+wind_info_t UG6464TDDBG01_Default_Window;
+
+UG2856KLBAG01_I2C::UG2856KLBAG01_I2C( void ) : hyperdisplay(UG2856KLBAG01_WIDTH, UG2856KLBAG01_HEIGHT), SSD1309_Arduino_I2C(UG2856KLBAG01_WIDTH, UG2856KLBAG01_HEIGHT)
+{
+
+}
+
+SSD1309_Status_t UG2856KLBAG01_I2C::begin(TwoWire &wirePort, bool sa0Val, uint8_t saoPin)
+{
+	// Call the functions to setup the super classes
+// Associate 
+	_sa0 = saoPin;
+	_i2c = &wirePort;
+	_sa0val = sa0Val;
+
+	// Set pinmodes
+	if(_sa0 != SSD1309_ARD_UNUSED_PIN){ pinMode(_sa0, OUTPUT); }
+
+	// Set pins to default positions
+	if(_sa0 != SSD1309_ARD_UNUSED_PIN){ digitalWrite(_sa0, _sa0val); }
+
+	// _i2c->begin(); // Moved into user's sketch
+
+	// Setup the default window
+	setWindowDefaults(pCurrentWindow);
+
+	// Power up the device
+	startup();	
+	defaultConfigure();
+
+	return SSD1309_Nominal;
+}
+
+SSD1309_Status_t UG2856KLBAG01_I2C::defaultConfigure( void )
+{
+	// This is the suggested initialization routine from WiseChip (pg. 9 of the module datasheet)
+	setCommandLock(false);
+	setPower(false); 
+
+	setDisplayClockDivideRatio(0xA0);
+	setMultiplexRatio(0x3F);
+	setDisplayOffset(0x00);
+	setDisplayStartLine(0x00);
+	setMemoryAddressingMode( 0x00 ); // Horizontal Addressing Mode
+	setSegmentMapping(true);
+	setCOMoutputDirection(true);
+	setCOMpinsHWconfig(0x12);
+	setContrastControl(0x8F);
+	setPreChargePeriod(0x25);
+	setVCOMHdeselectLevel(0x34);
+	overrideRam(false);
+	setInversion(false);
+	
+	// Clear the ram mirror and output to the display
+	memset( ramMirror, 0x00, (SSD1309_RAM_BYTES)*sizeof(SSD1309_Bite_t));
+	updateRefreshZone( UG2856KLBAG01_START_COL, UG2856KLBAG01_STOP_COL, UG2856KLBAG01_START_ROW, UG2856KLBAG01_STOP_ROW);		// Tell where we need updates (the whole screen)   
+	refreshDisplay();								// Perform updates
+
+	setPower(true); 
+
+  	return SSD1309_Nominal;
+}
+
+void UG2856KLBAG01_I2C::startup( void )
+{
+	// Assume that VDD and VCC are stable when this function is called
+	delay(20);
+	digitalWrite(_rst, LOW);
+	// delayMicroseconds(10);
+	delay(5);
+	digitalWrite(_rst, HIGH);
+	delay(200);
+	// Now you can do initialization
+}
+
+// void UG6464TDDBG01::getCharInfo(uint8_t val, char_info_t * pchar)
+// {
+// 	char_info_t * pcharinfo = &UG6464TDDBG01_Default_CharInfo;
+// 	// Do stuff to fill out the default char info structure, then return a pointer to it
+// 	pcharinfo->data = NULL;						// Color information for each pixel to be drawn
+// 	pcharinfo->xLoc = NULL;						// X location wrt upper-left corner of char location(cursor) for each pixel
+// 	pcharinfo->yLoc = NULL;						// Y location wrt upper-left corner of char location(cursor) for each pixel
+//     pcharinfo->numPixels = 0;					// Number of pixels contained in the data, xLoc, and yLoc arrays
+// 	pcharinfo->show = false;					// Whether or not to actually show the character
+// 	pcharinfo->causedNewline = false;			// Whether or not the character triggered/triggers a new line
+// }
+
+
+void UG2856KLBAG01_I2C::clearDisplay( void )
+{
+	// Store the old window pointer: 
+	wind_info_t * ptempWind = pCurrentWindow;
+
+	// Make a new default window
+	wind_info_t window;
+	pCurrentWindow = &window;
+
+	// Ensure the window is set up so that you can clear the whole screen
+	setWindowDefaults(&window);
+
+	// Make a local 'black' color 
+	uint8_t temp_buff[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};				// This buffer is long enough to represent '0' no matter what color mode is selected
+	
+	// Fill the temporary window with black
+	fillWindow((color_t)temp_buff);												// Pass the address of the buffer b/c we know it will be safe no matter what SSD1309 color mode is used
+
+	// Restore the previous window
+	pCurrentWindow = ptempWind;
+}
+
+void UG2856KLBAG01_I2C::setWindowDefaults(wind_info_t * pwindow)
+{
+	// // Fills out the default window structure with more or less reasonable defaults
+	// pwindow->xMin = UG6464TDDBG01_START_COL;
+	// pwindow->yMin = UG6464TDDBG01_START_ROW;
+	// pwindow->xMax = UG6464TDDBG01_STOP_COL;
+	// pwindow->yMax = UG6464TDDBG01_STOP_ROW;
+	// pwindow->cursorX = 0;							// cursor values are in window coordinates
+	// pwindow->cursorY = 0;
+	// pwindow->xReset = 0;
+	// pwindow->yReset = 0;
+	
+	// pwindow->lastCharacter.data = NULL;
+	// pwindow->lastCharacter.xLoc = NULL;
+	// pwindow->lastCharacter.yLoc = NULL;
+	// pwindow->lastCharacter.xDim = 0;
+	// pwindow->lastCharacter.yDim = 0;
+	// pwindow->lastCharacter.numPixels = 0;
+	// pwindow->lastCharacter.show = false;
+	// pwindow->lastCharacter.causesNewline = false;
+	
+	// pwindow->data = NULL;				// No window data yet
+	// setCurrentWindowColorSequence(NULL, 1, 0);	// Setup the default color (Which is NULL, so that you know it is not set yet)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+UG2856KLBAG01_SPI::UG2856KLBAG01_SPI( void ) : hyperdisplay(UG2856KLBAG01_WIDTH, UG2856KLBAG01_HEIGHT), SSD1309_Arduino_SPI(UG2856KLBAG01_WIDTH, UG2856KLBAG01_HEIGHT)
+{
+
+}
+
+SSD1309_Status_t UG2856KLBAG01_SPI::begin( uint8_t csPin, uint8_t dcPin, SPIClass &spiPort)
+{
+	// Call the functions to setup the super classes
+// Associate 
+	_cs = csPin;
+	_dc = dcPin;
+	_spi = &spiPort;
+
+	// Set pinmodes
+	pinMode(_cs, OUTPUT);
+	pinMode(_dc, OUTPUT);
+
+	// Set pins to default positions
+	digitalWrite(_cs, HIGH);
+	digitalWrite(_dc, HIGH);
+
+	// Setup the default window
+	setWindowDefaults(pCurrentWindow);
+
+	// Kick the SPI hardware into the right mode
+	_spi->beginTransaction(SPISettings(UG2856KLBAG01_SPI_FREQ_MAX, UG2856KLBAG01_SPI_ORDER, UG2856KLBAG01_SPI_MODE));
+	_spi->transfer(0x00);
+	_spi->endTransaction();
+
+	// Power up the device
+	startup();	
+	defaultConfigure();
+
+	return SSD1309_Nominal;
+}
+
+SSD1309_Status_t UG2856KLBAG01_SPI::defaultConfigure( void )
+{
+	// This is the suggested initialization routine from WiseChip (pg. 9 of the module datasheet)
+	setCommandLock(false);
+	setPower(false); 
+
+	setDisplayClockDivideRatio(0xA0);
+	setMultiplexRatio(0x3F);
+	setDisplayOffset(0x00);
+	setDisplayStartLine(0x00);
+	setMemoryAddressingMode( 0x00 ); // Horizontal Addressing Mode
+	setSegmentMapping(true);
+	setCOMoutputDirection(true);
+	setCOMpinsHWconfig(0x12);
+	setContrastControl(0x8F);
+	setPreChargePeriod(0x25);
+	setVCOMHdeselectLevel(0x34);
+	overrideRam(false);
+	setInversion(false);
+	
+	// Clear the ram mirror and output to the display
+	memset( ramMirror, 0x00, (SSD1309_RAM_BYTES)*sizeof(SSD1309_Bite_t));
+	updateRefreshZone( UG2856KLBAG01_START_COL, UG2856KLBAG01_STOP_COL, UG2856KLBAG01_START_ROW, UG2856KLBAG01_STOP_ROW);		// Tell where we need updates (the whole screen)   
+	refreshDisplay();								// Perform updates
+
+	setPower(true); 
+
+  	return SSD1309_Nominal;
+}
+
+void UG2856KLBAG01_SPI::startup( void )
+{
+	// Assume that VDD and VCC are stable when this function is called
+	if(_rst )
+	delay(20);
+	digitalWrite(_rst, LOW);
+	// delayMicroseconds(10);
+	delay(5);
+	digitalWrite(_rst, HIGH);
+	delay(200);
+	// Now you can do initialization
+}
+
+// void UG6464TDDBG01::getCharInfo(uint8_t val, char_info_t * pchar)
+// {
+// 	char_info_t * pcharinfo = &UG6464TDDBG01_Default_CharInfo;
+// 	// Do stuff to fill out the default char info structure, then return a pointer to it
+// 	pcharinfo->data = NULL;						// Color information for each pixel to be drawn
+// 	pcharinfo->xLoc = NULL;						// X location wrt upper-left corner of char location(cursor) for each pixel
+// 	pcharinfo->yLoc = NULL;						// Y location wrt upper-left corner of char location(cursor) for each pixel
+//     pcharinfo->numPixels = 0;					// Number of pixels contained in the data, xLoc, and yLoc arrays
+// 	pcharinfo->show = false;					// Whether or not to actually show the character
+// 	pcharinfo->causedNewline = false;			// Whether or not the character triggered/triggers a new line
+// }
+
+
+void UG2856KLBAG01_SPI::clearDisplay( void )
+{
+	// Store the old window pointer: 
+	wind_info_t * ptempWind = pCurrentWindow;
+
+	// Make a new default window
+	wind_info_t window;
+	pCurrentWindow = &window;
+
+	// Ensure the window is set up so that you can clear the whole screen
+	setWindowDefaults(&window);
+
+	// Make a local 'black' color 
+	uint8_t temp_buff[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};				// This buffer is long enough to represent '0' no matter what color mode is selected
+	
+	// Fill the temporary window with black
+	fillWindow((color_t)temp_buff);												// Pass the address of the buffer b/c we know it will be safe no matter what SSD1309 color mode is used
+
+	// Restore the previous window
+	pCurrentWindow = ptempWind;
+}
+
+void UG2856KLBAG01_SPI::setWindowDefaults(wind_info_t * pwindow)
+{
+	// // Fills out the default window structure with more or less reasonable defaults
+	// pwindow->xMin = UG6464TDDBG01_START_COL;
+	// pwindow->yMin = UG6464TDDBG01_START_ROW;
+	// pwindow->xMax = UG6464TDDBG01_STOP_COL;
+	// pwindow->yMax = UG6464TDDBG01_STOP_ROW;
+	// pwindow->cursorX = 0;							// cursor values are in window coordinates
+	// pwindow->cursorY = 0;
+	// pwindow->xReset = 0;
+	// pwindow->yReset = 0;
+	
+	// pwindow->lastCharacter.data = NULL;
+	// pwindow->lastCharacter.xLoc = NULL;
+	// pwindow->lastCharacter.yLoc = NULL;
+	// pwindow->lastCharacter.xDim = 0;
+	// pwindow->lastCharacter.yDim = 0;
+	// pwindow->lastCharacter.numPixels = 0;
+	// pwindow->lastCharacter.show = false;
+	// pwindow->lastCharacter.causesNewline = false;
+	
+	// pwindow->data = NULL;				// No window data yet
+	// setCurrentWindowColorSequence(NULL, 1, 0);	// Setup the default color (Which is NULL, so that you know it is not set yet)
+}
---
+++ src/HyperDisplay_UG2856KLBAG01.h
@@ -0,0 +1,89 @@
+/*
+
+A library to control SSD1309 OLED drivers, derived from the SparkFun HyperDisplay library
+
+Created: August 2018
+Modified: August 2018
+
+Authors: Owen Lyke
+
+*/
+
+#ifndef _HPYERDISPLAY_UG2856KLBAG01_H_
+#define _HPYERDISPLAY_UG2856KLBAG01_H_
+
+
+////////////////////////////////////////////////////////////
+//							Includes    				  //
+////////////////////////////////////////////////////////////
+#include "HyperDisplay_SSD1309.h"
+
+
+
+////////////////////////////////////////////////////////////
+//				UG2856KLBAG01 I2C Display Class   		  //
+////////////////////////////////////////////////////////////
+
+#define UG2856KLBAG01_WIDTH 	128
+#define UG2856KLBAG01_HEIGHT 	64
+#define UG2856KLBAG01_START_ROW	0x00
+#define UG2856KLBAG01_START_COL	0x00
+#define UG2856KLBAG01_STOP_ROW 	0x3F
+#define UG2856KLBAG01_STOP_COL	0x7F
+#define UG2856KLBAG01_START_PG 	0x00
+#define UG2856KLBAG01_STOP_PG	0x07
+
+class UG2856KLBAG01_I2C : public SSD1309_Arduino_I2C{							// This is a particular display from WiseChip. Get it here: 
+private:
+protected:
+public:
+	UG2856KLBAG01_I2C( void );	// Note that since the hardware screen choice (UG2856KLBAG01) constrained the dimensions there are no more free parameters to pass to the initialization
+
+	SSD1309_Status_t begin(TwoWire &wirePort = Wire, bool sa0Val = false, uint8_t saoPin = SSD1309_ARD_UNUSED_PIN);
+	SSD1309_Status_t defaultConfigure( void ); // The reccomended settings from the datasheet
+	void startup( void );		// The default startup for this particular display
+
+	// void getCharInfo(uint8_t val, char_info_t * pchar);
+
+	// Some specialized drawing frunctions
+	void clearDisplay( void );
+
+	// Special Functions
+	void setWindowDefaults(wind_info_t * pwindow); // Overrrides default implementation in hyperdisplay class
+};
+
+
+
+
+
+
+////////////////////////////////////////////////////////////
+//				UG2856KLBAG01 SPI Display Class   		  //
+////////////////////////////////////////////////////////////
+
+#define UG2856KLBAG01_SPI_FREQ_MAX 	100000
+// #define UG2856KLBAG01_SPI_FREQ_MAX 	1000000
+#define UG2856KLBAG01_SPI_MODE 		SPI_MODE3
+#define UG2856KLBAG01_SPI_ORDER		MSBFIRST
+
+class UG2856KLBAG01_SPI : public SSD1309_Arduino_SPI{							// This is a particular display from WiseChip. Get it here: 
+private:
+protected:
+public:
+	UG2856KLBAG01_SPI( void );	// Note that since the hardware screen choice (UG2856KLBAG01) constrained the dimensions there are no more free parameters to pass to the initialization
+
+	SSD1309_Status_t begin( uint8_t csPin, uint8_t dcPin, SPIClass &spiPort = SPI);
+	SSD1309_Status_t defaultConfigure( void ); // The reccomended settings from the datasheet
+	void startup( void );		// The default startup for this particular display
+
+	// void getCharInfo(uint8_t val, char_info_t * pchar);
+
+	// Some specialized drawing frunctions
+	void clearDisplay( void );
+
+	// Special Functions
+	void setWindowDefaults(wind_info_t * pwindow); // Overrrides default implementation in hyperdisplay class
+};
+
+
+#endif /* _HPYERDISPLAY_UG2856KLBAG01_H_ */
