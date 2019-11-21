66cf4a6b50c272efe841f2b20ac43868a7862bb9
Owen L - SFE <oclyke@gmail.com>
2019-11-21 11:06:57 -0700

    remove duplicate macro

--- src/HyperDisplay_UG2856KLBAG01.h
+++ src/HyperDisplay_UG2856KLBAG01.h
@@ -61,7 +61,6 @@ public:
 //				UG2856KLBAG01 SPI Display Class   		  //
 ////////////////////////////////////////////////////////////
 
-#define UG2856KLBAG01_SPI_FREQ_MAX 	1000000
 #define UG2856KLBAG01_SPI_MODE 		SPI_MODE3
 #define UG2856KLBAG01_SPI_ORDER		MSBFIRST
 
